7ec3fcd00b4e5245839dd00aad0c40c620b85e7b
bella922 <37675310+bella922@users.noreply.github.com>
2019-05-19 23:30:07 +1200

    Update 345group.cpp
    each tetris has 4 blocks
    each block has it own random color;

--- 345group.cpp
+++ 345group.cpp
@@ -1,72 +1,76 @@
 #include "pch.h"
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <iostream>
+using namespace std;
 using namespace sf;
 
-const int M = 20;
-const int N = 10;
+
+const int M = 20;//horizont Block size
+const int N = 10;//verticle block size
 
 int field[M][N] = {0};
 
 struct Point
-{
-    int x, y;
-} a[4], b[4];
+{int x,y;} a[4], b[4];
 
+//define tetris shapes
 int figures[7][4] =
-    {
-        1, 3, 5, 7, // I
-        2, 4, 5, 7, // Z
-        3, 5, 4, 6, // S
-        3, 5, 4, 7, // T
-        2, 3, 5, 7, // L
-        3, 5, 7, 6, // J
-        2, 3, 4, 5, // O
+{
+	1,3,5,7, // I
+	2,4,5,7, // Z
+	3,5,4,6, // S
+	3,5,4,7, // T
+	2,3,5,7, // L
+	3,5,7,6, // J
+	2,3,4,5, // O
 };
 
+//check for boundaries and collision with other shapes
 bool check()
 {
-    for (int i = 0; i < 4; i++)
-        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M)
-            return 0;
-        else if (field[a[i].y][a[i].x])
-            return 0;
+   for (int i=0;i<4;i++)
+	  if (a[i].x<0 || a[i].x>=N || a[i].y>=M) return 0;
+      else if (field[a[i].y][a[i].x]) return 0;
 
-    return 1;
+   return 1;
 };
 
-void initShape(sf::RectangleShape &shape, sf::Vector2f const &pos, sf::Color const &color)
-{
-    shape.setFillColor(color);
-    shape.setPosition(pos);
-    shape.setOrigin(shape.getSize() * 0.5f); // The center of the rectangle
+void initShape(sf::RectangleShape &shape, sf::Vector2f const &pos, sf::Color const &color) {
+	shape.setFillColor(color);
+	shape.setPosition(pos);
+	shape.setOrigin(shape.getSize() * 0.5f); // The center of the rectangle
 }
+
+
 
 int main()
 {
-    srand(time(0));
+  srand(time(0));	 
 
-    RenderWindow window(sf::VideoMode(350, 450), "Advanced Tetris"); //(VideoMode(320, 480), "The Game!");
+	RenderWindow window(sf::VideoMode(1024, 1000), "Advanced Tetris");//(VideoMode(320, 480), "The Game!");
 
-    Texture t1, t2, t3;
-    t1.loadFromFile("images/tiles.png");
-    t2.loadFromFile("images/background.png");
-    t3.loadFromFile("images/frame.png");
-    Sprite s(t1), background(t2), frame(t3);
+  Texture t1,t2,t3;
+	t1.loadFromFile("images/ntiles.png");
+	t2.loadFromFile("images/background.png");
+	t3.loadFromFile("images/frame.png");
+	Sprite s(t1), background(t2), frame(t3);
 
-    int dx = 0;
-    bool rotate = 0;
-    int colorNum = 1;
-    float timer = 0, delay = 0.3;
 
-    Clock clock;
+  int distance_x=0; //define x coordinate of blocks
+  bool is_rotate=0; //define if rotate or not
+  int color_index=2;//define color index of image
+	float timer=0;
+  float delay=0.3; //define speed for gravity
 
-    window.setFramerateLimit(10);
+	Clock clock;
+
+
+	 window.setFramerateLimit(10);
     // sf::WindowHandle handle = window.getSystemHandle();
     sf::Font font;
-    if (!font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf"))
-    {
-        window.setTitle("Font Error");
+    if (!font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf")) {
+      window.setTitle("Font Error");
     }
 
     // set score title
@@ -79,7 +83,7 @@ int main()
     scoreTitle.setCharacterSize(24);
     // set the color
     scoreTitle.setFillColor(sf::Color::Red);
-    scoreTitle.setPosition(240, 15);
+    scoreTitle.setPosition(800,50);
 
     // set score text
     sf::Text strScore;
@@ -92,7 +96,7 @@ int main()
     strScore.setCharacterSize(24);
     // set the color
     strScore.setFillColor(sf::Color::Red);
-    strScore.setPosition(240, 30);
+    strScore.setPosition(800,100);
 
     // set cancelled title
     sf::Text cancTitle;
@@ -104,7 +108,7 @@ int main()
     cancTitle.setCharacterSize(24);
     // set the color
     cancTitle.setFillColor(sf::Color::Red);
-    cancTitle.setPosition(240, 45);
+    cancTitle.setPosition(800,150);
 
     // set cancelled text
     sf::Text cancScore;
@@ -117,7 +121,7 @@ int main()
     cancScore.setCharacterSize(24);
     // set the color
     cancScore.setFillColor(sf::Color::Red);
-    cancScore.setPosition(240, 60);
+    cancScore.setPosition(800,200);
 
     // set level title
     sf::Text levelTitle;
@@ -129,7 +133,7 @@ int main()
     levelTitle.setCharacterSize(24);
     // set the color
     levelTitle.setFillColor(sf::Color::Red);
-    levelTitle.setPosition(240, 75);
+    levelTitle.setPosition(800,250);
 
     // set level text
     sf::Text strLevel;
@@ -142,26 +146,26 @@ int main()
     strLevel.setCharacterSize(24);
     // set the color
     strLevel.setFillColor(sf::Color::Red);
-    strLevel.setPosition(240, 90);
+    strLevel.setPosition(800,300);
 
     //main playing area container
-    sf::RectangleShape container(sf::Vector2f(203, 400));
+    sf::RectangleShape container(sf::Vector2f(411, 830));
     container.setFillColor(sf::Color::White);
-    container.setPosition(25, 30);
+    container.setPosition(179,130);
 
     //enemy and player
-    sf::Vector2f enemyStartPos = sf::Vector2f(45, 15);
-    sf::RectangleShape enemyRect(sf::Vector2f(15, 15));
+    sf::Vector2f enemyStartPos = sf::Vector2f(150, 50);
+    sf::RectangleShape enemyRect(sf::Vector2f(50, 50));
     initShape(enemyRect, enemyStartPos, sf::Color::Red);
-    sf::Vector2f playerStartPos = sf::Vector2f(90, 15);
-    sf::RectangleShape playerRect(sf::Vector2f(15, 15));
+    sf::Vector2f playerStartPos = sf::Vector2f(300, 50);
+    sf::RectangleShape playerRect(sf::Vector2f(50, 50));
     initShape(playerRect, playerStartPos, sf::Color::Blue);
 
     while (window.isOpen())
     {
-        float time = clock.getElapsedTime().asSeconds();
-        clock.restart();
-        timer += time;
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer+=time;
 
         Event e;
         while (window.pollEvent(e))
@@ -169,154 +173,164 @@ int main()
             if (e.type == Event::Closed)
                 window.close();
 
-            if (e.type == Event::KeyPressed)
-                if (e.key.code == Keyboard::Up)
-                    rotate = true;
-                else if (e.key.code == Keyboard::Left)
-                    dx = -1;
-                else if (e.key.code == Keyboard::Right)
-                    dx = 1;
-        }
+			if (e.type == Event::KeyPressed)
+			  if (e.key.code==Keyboard::Up) is_rotate=true;
+			  else if (e.key.code==Keyboard::Left) distance_x=-1;
+			  else if (e.key.code==Keyboard::Right) distance_x=1;
+		}
 
-        if (Keyboard::isKeyPressed(Keyboard::Down))
-            delay = 0.05;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Insert))
-        {
-            /* for test only, press insert to get score,
+	if (Keyboard::isKeyPressed(Keyboard::Down)) delay=0.05;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Insert)) {
+			/* for test only, press insert to get score,
 				 cancellation, move player*/
             enemyRect.move(-1, 0);
-            playerRect.move(1, 0);
+            playerRect.move(1,0);
             score += 10;
             strScore.setString(std::to_string(score));
             canc++;
             cancScore.setString(std::to_string(canc));
         }
         // Enemy always moving
-        enemyRect.move(1, 0);
+        enemyRect.move(1, 0); 
         // Enemy got you
         if (playerRect.getGlobalBounds()
-                .intersects(enemyRect.getGlobalBounds()))
-        {
+	    .intersects(enemyRect.getGlobalBounds())) {
             enemyRect.setPosition(enemyStartPos);
         }
-        if (playerRect.getPosition().x >= 450)
-        {
+        if(playerRect.getPosition().x >= 450) {
             level++;
             strLevel.setString(std::to_string(level));
             enemyRect.setPosition(enemyStartPos);
             playerRect.setPosition(playerStartPos);
         }
 
-        //// <- Move -> ///
-        for (int i = 0; i < 4; i++)
-        {
-            b[i] = a[i];
-            a[i].x += dx;
-        }
-        if (!check())
-            for (int i = 0; i < 4; i++)
-                a[i] = b[i];
-
-        //////Rotate//////
-        if (rotate)
-        {
-            Point p = a[1]; //center of rotation
-            for (int i = 0; i < 4; i++)
-            {
-                int x = a[i].y - p.y;
-                int y = a[i].x - p.x;
-                a[i].x = p.x - x;
-                a[i].y = p.y + y;
-            }
-            if (!check())
-                for (int i = 0; i < 4; i++)
-                    a[i] = b[i];
-        }
-
-        ///////Tick//////
-
-        if (timer > delay)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                b[i] = a[i];
-                a[i].y += 1;
-            }
-
-            if (!check())
-            {
-                for (int i = 0; i < 4; i++)
-                    field[b[i].y][b[i].x] = colorNum;
-
-                colorNum = 1 + rand() % 7;
-                int n = rand() % 7;
-                for (int i = 0; i < 4; i++)
-                {
-                    a[i].x = figures[n][i] % 2;
-                    a[i].y = figures[n][i] / 2;
-                }
-            }
-
-            timer = 0;
-        }
-
-        ///////check lines//////////
-
-        int k = M - 1;
-        for (int i = M - 1; i > 0; i--)
-        {
-            int count = 0;
-            for (int j = 0; j < N; j++)
-            {
-                if (field[i][j])
-                    count++;
-                field[k][j] = field[i][j];
-            }
-            if (count < N)
-                k--;
-        }
-
-        dx = 0;
-        rotate = 0;
-        delay = 0.3;
-
-        /////////draw//////////
-        //window.clear(Color::White);
-        window.clear();
-        //window.draw(background);
-        window.draw(playerRect);
-        window.draw(enemyRect);
-        window.draw(container);
-        window.draw(scoreTitle);
-        window.draw(strScore);
-        window.draw(cancTitle);
-        window.draw(cancScore);
-        window.draw(levelTitle);
-        window.draw(strLevel);
-
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++)
-            {
-                if (field[i][j] == 0)
-                    continue;
-                s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-                s.setPosition(j * 20, i * 20);
-                //s.move(28,31); //offset
-                s.move(28, 31); //offset
-                window.draw(s);
-            }
-
-        for (int i = 0; i < 4; i++)
-        {
-            s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-            s.setPosition(a[i].x * 20, a[i].y * 20);
-            s.move(28, 31); //offset
-            window.draw(s);
-        }
-
-        //window.draw(frame);
-        window.display();
+	//left and right movement
+	for (int i=0;i<4;i++)  { 
+    b[i]=a[i]; 
+    a[i].x+=distance_x;
+  }
+  if (!check()){
+    for (int i=0;i<4;i++) {
+      a[i]=b[i];
     }
+  }
+
+	//totation
+	if (is_rotate){
+		Point p = a[1]; //center of rotation
+		for (int i=0;i<4;i++){
+			int x = a[i].y-p.y;
+			int y = a[i].x-p.x;
+			a[i].x = p.x - x;
+			a[i].y = p.y + y;
+	 	}
+   	if (!check()) {
+      for (int i=0;i<4;i++) {
+        a[i]=b[i];
+      }
+    }
+	}
+
+	///////Tick//////
+	int cc[4];
+	if (timer>delay){
+	    for (int i=0;i<4;i++) { 
+        b[i]=a[i]; 
+        a[i].y+=1; 
+      }
+
+      if (!check()){
+       //for (int i=0;i<4;i++) field[b[i].y][b[i].x]=color_index;
+        for (int i=0;i<4;i++) {
+          field[b[i].y][b[i].x]=cc[i];
+        }
+        color_index=1+rand()%5;//update color index from 1 to 6
+        
+        //initialise different tetris, n ranged from 0-6
+        int n=rand()%7;
+        for (int i=0;i<4;i++){
+            a[i].x = figures[n][i] % 2;
+            a[i].y = figures[n][i] / 2;
+        }
+      }
+	  	timer=0;
+	  }
+
+	//elimination
+  int k=M-1;
+	for (int i=M-1;i>0;i--){
+		int count=0;
+		for (int j=0;j<N;j++){
+		    if (field[i][j]) {
+          count++;
+        }
+		    field[k][j]=field[i][j];
+		}
+		if (count<N) {
+      k--;
+    }
+	}
+
+  distance_x=0; 
+  is_rotate=0; 
+  delay=0.3;
+
+  //drawing elements
+  //window.clear(Color::White);	
+	window.clear();
+  //window.draw(background);
+	window.draw(playerRect);
+  window.draw(enemyRect);
+  window.draw(container);
+  window.draw(scoreTitle);
+  window.draw(strScore);
+  window.draw(cancTitle);
+  window.draw(cancScore);
+  window.draw(levelTitle);
+  window.draw(strLevel);
+		  
+	for (int i=0;i<M;i++)
+	 for (int j=0;j<N;j++)
+	   {
+         if (field[i][j]==0) {
+           continue;
+         }
+		 s.setTextureRect(IntRect(field[i][j]*100,40,40,40));
+		 s.setPosition(j*41,i*41);
+		 s.move(180, 140); //offset, the point generate a new shape
+		 window.draw(s);
+	   }
+
+	for (int i=0;i<4;i++)
+	  {
+        cc[i] = color_index;
+          if(i==0){
+            if(cc[i]<=6){
+              cc[i]++;
+            }
+          }else if(i==2){
+            if(cc[i]>=2){
+              cc[i]--;
+            }
+          }else if(i==2){
+            if(cc[i]<=5){
+            cc[i]+=2;
+            }
+          }else if(i==3){
+            if(cc[i]>=3){
+            cc[i]-=2;
+            }
+          }
+		
+    s.setTextureRect(IntRect(cc[i]*100,40,40,40));
+		s.setPosition(a[i].x*41,a[i].y*41);
+		s.move(180,140); //offset,the point generate a new shape
+		window.draw(s);
+	  }
+
+	//window.draw(frame);
+ 	window.display();
+	}
 
     return 0;
 }
